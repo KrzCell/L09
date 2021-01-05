9438f134a69691de553812448b340b29e1e81062
Krzysztof Cel <krzycel789@student.polsl.pl>
2021-01-05 17:12:02 +0100

    zadanieB-koniec

--- L09.cpp
+++ L09.cpp
@@ -138,15 +138,9 @@ int wyszukajPierwsza(double* tablica, int rozmiar, double wartSzukana)
 	throw std::domain_error("Wartosc nie wystąpila");
 }
 
-int ileRazyZnak(const char* text, char znak)
-{
-	int liczbaWystapien = 0;
-	while (*text++)
-		if (*text == znak) liczbaWystapien++;
-	return liczbaWystapien;
-}
+//
 
-void test_1()
+void test_pozycja_wartosci()
 {
 	double tab[] = { 1.0,1.1,0.3,4.0,5.0 };
 	int rozmiar = sizeof(tab);
@@ -158,18 +152,130 @@ void test_1()
 		cerr << "Blad";
 }
 
-void test_2()
+void test_zwrot_wart_wiecej_niz_1raz()
 {
-	double tab[] = { 1.0,1.1,0.3,4.0,5.0 };
-	int i = 0;
-	
+	double tab[] = { 1.0,1.0,2.0,0.3,4.0,5.0 };
+	int rozmiar = sizeof(tab);
+	double wartosc_szukana = 4.0;
+	int oczekiwana_wart = wyszukajPierwsza(tab,rozmiar,wartosc_szukana);
+	if(oczekiwana_wart == wartosc_szukana)
+		cerr << "Ok";
+	else
+		cerr << "Blad";
+}
+
+void test_blad_adresu()
+{
+	double tab[] = { 1.0,1.0,0.3,4.0,5.0 };
+	int rozmiar = sizeof(tab);
+	double wartosc_szukana = 4.0;
+	try {
+		wyszukajPierwsza(nullptr, rozmiar, wartosc_szukana);
+	}
+	catch (invalid_argument) {
+		cerr << "Ok";
+		return;
+	}catch(...){}
+	cerr << "Blad";
+}
+
+void test_blad_rozmiaru()
+{
+	double tab[] = { 1.0,1.0,0.3,4.0,5.0 };
+	int rozmiar = -1;
+	double wartosc_szukana = 4.0;
+	try {
+		wyszukajPierwsza(tab, rozmiar, wartosc_szukana);
+	}
+	catch (length_error) {
+		cerr << "Ok";
+		return;
+	}
+	catch (...) {}
+	cerr << "Blad";
+}
+
+void test_brak_wartosci()
+{
+	double tab[] = { 1.0,1.0,0.3,4.0,5.0 };
+	int rozmiar = sizeof(tab);
+	double wartosc_szukana = 6;
+	try {
+		wyszukajPierwsza(tab, rozmiar, wartosc_szukana);
+	}
+	catch (domain_error) {
+		cerr << "Ok";
+		return;
+	}
+	catch (...) {}
+	cerr << "Blad";
+}
+
+//
+
+int ileRazyZnak(const char* text, char znak)
+{
+	int liczbaWystapien = 0;
+	while (*text)
+		if (*text++ == znak) liczbaWystapien++;
+	return liczbaWystapien;
+}
+
+//
+
+void test2_liczenie_znakow()
+{
+	char text[] = "jedenjeden";
+	int oczekiwana_wartosc = 4;
+	if (ileRazyZnak(text, 'e') == oczekiwana_wartosc)
+		cerr << "Ok";
+	else
+		cerr << "Blad";
+}
+
+void test2_pierwsza_litera()
+{
+	char text[] = "jedenjeden";
+	char oczekiwana_wartosc = 'j';
+	if (text[0] == oczekiwana_wartosc)
+		cerr << "Ok";
+	else
+		cerr << "Blad";
+}
+
+void test2_brak_znaku_w_txt()
+{
+	char text[] = "jedenjeden";
+	char oczekiwana_wartosc = 'w';
+	if (ileRazyZnak(text, oczekiwana_wartosc) == 0)
+		cerr << "Ok";
+	else
+		cerr << "Blad";
 }
 
 int main()
 {
 	double tab[] = { 1.0,1.1,0.3,4.0,5.0 };
-	cout<<wyszukajPierwsza(tab, 5, 4.0)<<endl;
-	test_1;
+	//cout<<wyszukajPierwsza(tab, 5, 4.0)<<endl;
+	
+	cout << "Funkcja wyszukajPierwsza()" << endl << "Test 1 - ";
+	test_pozycja_wartosci();
+	cout << endl << "Test 2 - ";
+	test_zwrot_wart_wiecej_niz_1raz();
+	cout << endl << "Test 3 - ";
+	test_blad_adresu();
+	cout << endl << "Test 4 - ";
+	test_blad_rozmiaru();
+	cout << endl << "Test 5 - ";
+	test_brak_wartosci();
+
+	char text[] = "jedenjeden";
+	cout << endl << "Funkcja ileRazyZnak()" << endl << "Test 1 - ";
+	test2_liczenie_znakow();
+	cout << endl << "Test 2 - ";
+	test2_pierwsza_litera();
+	cout << endl << "Test 3 - ";
+	test2_brak_znaku_w_txt();
 }
 
 #endif
